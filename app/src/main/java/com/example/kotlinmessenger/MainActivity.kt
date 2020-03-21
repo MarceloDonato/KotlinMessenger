73229a42f46d137e618a93b931cc28538b3da841
MarceloDonato <marcelo_donatoo@hotmail.com>
2020-03-21 14:12:46 -0300

    Initial commit

---
+++ app/src/main/java/com/example/kotlinmessenger/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.kotlinmessenger
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        register_button_register.setOnClickListener {
+            val email = email_edittext_register.text.toString()
+            val password = password_edittext_register.text.toString()
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "please enter text in emailpasswor", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            Log.d("MainActivity", "Email is " + email)
+            Log.d("MainActivity", "Password: $password")
+
+            //firebase authentication to create a user with email and password
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                  if (it.isSuccessful) return@addOnCompleteListener
+
+                    //else if successful
+                    Log.d("Main", "Successfully created user with uid: ${it.result.user.uid}")
+                }
+        }
+
+        already_have_an_account_text_View.setOnClickListener {
+            Log.d("MainActivity", "Try to show login activity")
+
+            // launch the login activity somehow
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+
+        }
+
+    }
+}
