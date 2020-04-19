854d5d988cb24fecc7d3d6534f9ab1566650b565
MarceloDonato <marcelo_donatoo@hotmail.com>
2020-04-19 20:05:06 -0300

    add lastest_messages_row

--- app/src/main/java/com/example/kotlinmessenger/models/ChatMessage.kt
+++ app/src/main/java/com/example/kotlinmessenger/models/ChatMessage.kt
@@ -1,2 +1,6 @@
 package com.example.kotlinmessenger.models
 
+class chatMessage(val id: String, val text: String, val fromId: String, val toId: String,
+                  timestamp: Long) {
+    constructor() : this("","","","",-1)
+}
